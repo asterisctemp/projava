f16ee89467cc710fcea42278a88ac8625ee3ecc2
chandrakant <asterisctemp@gmail.com>
2022-07-08 07:15:47 -0700

    thrid commit i create div pro

--- Test.java
+++ Test.java
@@ -2,8 +2,8 @@ class Test {
 
     public static void main(String ar[]) {
 
-        int a = 10, b = 20;
-        int c = a + b;
+        int a = 10, b = 0;
+        int c = a / b;
         System.out.println("Result " + c);
 
     }
